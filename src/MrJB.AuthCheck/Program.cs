b9663f476bb4157590a22fd8da703470a710d2b8
mrjamiebowman <jamie@redoma.digital>
2026-05-08 21:06:03 -0500

    serilog

--- src/MrJB.AuthCheck/Program.cs
+++ src/MrJB.AuthCheck/Program.cs
@@ -2,8 +2,28 @@ using MrJB.AuthCheck;
 using MrJB.AuthCheck.Domain.Configuration;
 using MrJB.AuthCheck.Domain.Interfaces;
 using MrJB.AuthCheck.Services;
+using Serilog;
+using Serilog.Events;
+using Serilog.Sinks.SystemConsole.Themes;
 
 var builder = WebApplication.CreateBuilder(args);
+
+// logger
+Log.Logger = new LoggerConfiguration()
+    .MinimumLevel.Information()
+    .MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
+    .MinimumLevel.Override("Microsoft.AspNetCore", LogEventLevel.Warning)
+    .MinimumLevel.Override("System", LogEventLevel.Warning)
+    .Enrich.FromLogContext()
+    .WriteTo.Console(
+        theme: AnsiConsoleTheme.Code,
+        outputTemplate:
+            "[{Timestamp:HH:mm:ss} {Level:u3}] {SourceContext}{NewLine}" +
+            "{Message:lj}{NewLine}" +
+            "{Exception}")
+    .CreateLogger();
+
+builder.Host.UseSerilog();
 
 builder.AddServiceDefaults();
 
